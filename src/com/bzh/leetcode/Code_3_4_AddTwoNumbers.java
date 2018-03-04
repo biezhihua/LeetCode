7e4cd655f5c30ef7dadbb32811d781879f8a9d51
biezhihua <biezhihua@gmail.com>
2018-03-04 16:49:25 +0800

    Simple Add Two Numbers

--- src/com/bzh/leetcode/Code_3_4_AddTwoNumbers.java
+++ src/com/bzh/leetcode/Code_3_4_AddTwoNumbers.java
@@ -42,16 +42,12 @@ public class Code_3_4_AddTwoNumbers {
         ListNode tmp = null;
         ListNode result = null;
 
+        int carry = 0;
+        while (l1 != null || l2 != null || carry != 0) {
+            int sum = (l1 == null ? 0 : l1.val) + (l2 == null ? 0 : l2.val) + carry;
+            carry = sum / 10;
 
-        boolean flag = false;
-
-        while (l1 != null && l2 != null) {
-
-            int cur = l1.val + l2.val + (flag ? 1 : 0);
-
-            flag = cur >= 10;
-
-            ListNode node = new ListNode(flag ? cur % 10 : cur);
+            ListNode node = new ListNode(sum % 10);
             if (tmp == null) {
                 tmp = node;
                 result = tmp;
@@ -60,48 +56,8 @@ public class Code_3_4_AddTwoNumbers {
                 tmp = tmp.next;
             }
 
-            l1 = l1.next;
-            l2 = l2.next;
-        }
-
-        while (l1 != null) {
-
-            int cur = l1.val + (flag ? 1 : 0);
-
-            flag = cur >= 10;
-
-            ListNode node = new ListNode(flag ? cur % 10 : cur);
-            if (tmp == null) {
-                tmp = node;
-                result = tmp;
-            } else {
-                tmp.next = node;
-                tmp = tmp.next;
-            }
-
-            l1 = l1.next;
-
-        }
-
-        while (l2 != null) {
-
-            int cur = l2.val + (flag ? 1 : 0);
-
-            flag = cur >= 10;
-
-            ListNode node = new ListNode(flag ? cur % 10 : cur);
-            if (tmp == null) {
-                tmp = node;
-                result = tmp;
-            } else {
-                tmp.next = node;
-                tmp = tmp.next;
-            }
-            l2 = l2.next;
-        }
-
-        if (flag) {
-            tmp.next = new ListNode(1);
+            l1 = l1 == null ? null : l1.next;
+            l2 = l2 == null ? null : l2.next;
         }
 
         return result;
