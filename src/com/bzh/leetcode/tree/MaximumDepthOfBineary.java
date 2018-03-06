4daa92d77f4b540d7a12e21faf05d1a33623e2df
biezhihua <biezhihua@gmail.com>
2018-03-06 17:35:14 +0800

    MaxDepth Tree

--- src/com/bzh/leetcode/tree/MaximumDepthOfBineary.java
+++ src/com/bzh/leetcode/tree/MaximumDepthOfBineary.java
@@ -1,5 +1,8 @@
 package com.bzh.leetcode.tree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class MaximumDepthOfBineary {
 
 
@@ -14,5 +17,35 @@ public class MaximumDepthOfBineary {
         int rightTreeDepth = maxDepth(root.right) + 1;
 
         return leftTreeDepth > rightTreeDepth ? leftTreeDepth : rightTreeDepth;
+    }
+
+
+    public int maxDepth2(TreeNode root) {
+
+        if (root == null) {
+            return 0;
+        }
+
+        int depth = 0;
+
+        Queue<TreeNode> qu = new LinkedList<>();
+
+        qu.offer(root);
+
+        while (!qu.isEmpty()) {
+            depth++;
+            int size = qu.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = qu.poll();
+                if (node.left != null) {
+                    qu.offer(node.left);
+                }
+                if (node.right != null) {
+                    qu.offer(node.right);
+                }
+            }
+        }
+
+        return depth;
     }
 }
