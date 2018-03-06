84d9d10125b25ad305065aa6feb2a49bf6e85851
biezhihua <biezhihua@gmail.com>
2018-03-06 15:55:45 +0800

    update

---
+++ src/com/bzh/leetcode/string/CountAndSay.java
@@ -0,0 +1,15 @@
+package com.bzh.leetcode.string;
+
+import org.junit.Test;
+
+public class CountAndSay {
+
+    @Test
+    public void test(){
+
+    }
+
+    public String countAndSay(int n) {
+        return "";
+    }
+}
---
+++ src/com/bzh/leetcode/string/LongestCommonPrefix.java
@@ -0,0 +1,15 @@
+package com.bzh.leetcode.string;
+
+import org.junit.Test;
+
+public class LongestCommonPrefix {
+
+    @Test
+    public void test() {
+
+    }
+
+    public String longestCommonPrefix(String[] strs) {
+        return "";
+    }
+}
