c9052b6e88da8208cb89d76c2cc9ec1810c3a668
biezhihua <biezhihua@gmail.com>
2018-03-06 16:18:04 +0800

    Merge LinkedList Simple2

--- src/com/bzh/leetcode/link/MergeTwoSortedLists.java
+++ src/com/bzh/leetcode/link/MergeTwoSortedLists.java
@@ -44,6 +44,18 @@ public class MergeTwoSortedLists {
         return dummy.next;
     }
 
+    public ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
+
     class ListNode {
         int val;
         ListNode next;
