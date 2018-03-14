8f987fcb7fa25773a9cf5e8c721055d19e839018
biezhihua <biezhihua@gmail.com>
2018-03-14 11:50:58 +0800

    update

--- src/com/bzh/leetcode/array/RotateArray.java
+++ src/com/bzh/leetcode/array/RotateArray.java
@@ -66,6 +66,7 @@ public class RotateArray {
     @Test
     public void test2() {
 
+
         // 1
         // 1, 2, 3, 4, 5, 6, 7
 
@@ -91,30 +92,112 @@ public class RotateArray {
         // 6, 7, 1, 2, 3, 4, 5
 
 
-        Assert.assertArrayEquals(new int[]{7, 1, 2, 3, 4, 5, 6}, rotate2(new int[]{1, 2, 3, 4, 5, 6, 7}, 1));
-        Assert.assertArrayEquals(new int[]{1, 2, 3, 4, 5, 6, 7}, rotate2(new int[]{1, 2, 3, 4, 5, 6, 7}, 0));
-        Assert.assertArrayEquals(new int[]{6, 7, 1, 2, 3, 4, 5}, rotate2(new int[]{1, 2, 3, 4, 5, 6, 7}, 2));
-        Assert.assertArrayEquals(new int[]{5, 6, 7, 1, 2, 3, 4}, rotate2(new int[]{1, 2, 3, 4, 5, 6, 7}, 3));
-        Assert.assertArrayEquals(new int[]{4, 5, 6, 7, 1, 2, 3}, rotate2(new int[]{1, 2, 3, 4, 5, 6, 7}, 4));
-        Assert.assertArrayEquals(new int[]{3, 4, 5, 6, 7, 1, 2}, rotate2(new int[]{1, 2, 3, 4, 5, 6, 7}, 5));
-        Assert.assertArrayEquals(new int[]{2, 3, 4, 5, 6, 7, 1}, rotate2(new int[]{1, 2, 3, 4, 5, 6, 7}, 6));
-        Assert.assertArrayEquals(new int[]{1, 2, 3, 4, 5, 6, 7}, rotate2(new int[]{1, 2, 3, 4, 5, 6, 7}, 7));
-        Assert.assertArrayEquals(new int[]{2, 1}, rotate2(new int[]{1, 2}, 1));
-        Assert.assertArrayEquals(new int[]{2, 1}, rotate2(new int[]{1, 2}, 3));
+        // 1
+        // 1, 2, 3, 4, 5, 6
+
+        //       3
+        // 1, 2, 1, 4, 5, 6
+
+        //             5
+        // 1, 2, 1, 4, 3, 6
+
+        //    2
+        // 5, 2, 1, 4, 3, 6
+
+        //          4
+        // 5, 2, 1, 2, 3, 6
+
+        //                6
+        // 5, 2, 1, 2, 3, 4
+
+        //    6
+        // 5, 6, 1, 2, 3, 4
+
+        int[] nums = {1, 2, 3, 4, 5, 6};
+
+//        rotate2(nums, 2);
+//        Assert.assertArrayEquals(new int[]{5, 6, 1, 2, 3, 4}, nums);
+//
+        nums = new int[]{1, 2, 3, 4, 5, 6, 7};
+        rotate2(nums, 1);
+        Assert.assertArrayEquals(new int[]{7, 1, 2, 3, 4, 5, 6}, nums);
+
+        nums = new int[]{1, 2, 3, 4, 5, 6, 7};
+        rotate2(nums, 0);
+        Assert.assertArrayEquals(new int[]{1, 2, 3, 4, 5, 6, 7}, nums);
+
+
+        nums = new int[]{1, 2, 3, 4, 5, 6, 7};
+        rotate2(nums, 2);
+        Assert.assertArrayEquals(new int[]{6, 7, 1, 2, 3, 4, 5}, nums);
+
+        nums = new int[]{1, 2, 3, 4, 5, 6, 7};
+        rotate2(nums, 3);
+        Assert.assertArrayEquals(new int[]{5, 6, 7, 1, 2, 3, 4}, nums);
+
+        nums = new int[]{1, 2, 3, 4, 5, 6, 7};
+        rotate2(nums, 4);
+        Assert.assertArrayEquals(new int[]{4, 5, 6, 7, 1, 2, 3}, nums);
+
+        nums = new int[]{1, 2, 3, 4, 5, 6, 7};
+        rotate2(nums, 5);
+        Assert.assertArrayEquals(new int[]{3, 4, 5, 6, 7, 1, 2}, nums);
+
+        nums = new int[]{1, 2, 3, 4, 5, 6, 7};
+        rotate2(nums, 6);
+        Assert.assertArrayEquals(new int[]{2, 3, 4, 5, 6, 7, 1}, nums);
+
+        nums = new int[]{1, 2, 3, 4, 5, 6, 7};
+        rotate2(nums, 7);
+        Assert.assertArrayEquals(new int[]{1, 2, 3, 4, 5, 6, 7}, nums);
+
+        nums = new int[]{1, 2};
+        rotate2(nums, 1);
+        Assert.assertArrayEquals(new int[]{2, 1}, nums);
+
+        nums = new int[]{1, 2};
+        rotate2(nums, 3);
+        Assert.assertArrayEquals(new int[]{2, 1}, nums);
+
+        // -1 -100 3 99
+
+        // 99 -100 3 -1
+
+        // -100 99 3 -1
+
+        // -100 3 99 -1
+
+        nums = new int[]{-1, -100, 3, 99};
+        rotate2(nums, 3);
+        Assert.assertArrayEquals(new int[]{-100, 3, 99, -1}, nums);
+
+        nums = new int[]{1, 2};
+        rotate2(nums, 1);
+        Assert.assertArrayEquals(new int[]{2, 1}, nums);
     }
 
-    public int[] rotate2(int[] nums, int k) {
-        int pos = 0;
-        int posValue = nums[pos];
-        int index = 0;
-        while (index++ < nums.length) {
-            pos = (pos + k) % nums.length;
-            int tmp = nums[pos];
-            nums[pos] = posValue;
-            posValue = tmp;
+    public void rotate2(int[] nums, int k) {
+        if (nums.length == 0 || (k %= nums.length) == 0) {
+            return;
         }
+        int length = nums.length;
+        int start = 0;
+        int i = 0;
+        int cur = nums[i];
+        int cnt = 0;
 
-        return nums;
+        while (cnt++ < length) {
+            i = (i + k) % length;
+            int t = nums[i];
+            nums[i] = cur;
+            if (i == start) {
+                ++start;
+                ++i;
+                cur = nums[i];
+            } else {
+                cur = t;
+            }
+        }
     }
 
 }
