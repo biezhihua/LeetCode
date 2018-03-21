d63aac84267ea55d09c5633bfd1c05b62bf70634
biezhihua <biezhihua@gmail.com>
2018-03-21 23:54:57 +0800

    Valide Sudoku

--- src/com/bzh/leetcode/array/ValidSudoKu.java
+++ src/com/bzh/leetcode/array/ValidSudoKu.java
@@ -5,5 +5,30 @@ import org.junit.Test;
 
 public class ValidSudoKu {
 
+    @Test
+    public void test() {
+        char[][] a = {{'.', '8', '7', '6', '5', '4', '3', '2', '1'}, {'2', '.', '.', '.', '.', '.', '.', '.', '.'}, {'3', '.', '.', '.', '.', '.', '.', '.', '.'}, {'4', '.', '.', '.', '.', '.', '.', '.', '.'}, {'5', '.', '.', '.', '.', '.', '.', '.', '.'}, {'6', '.', '.', '.', '.', '.', '.', '.', '.'}, {'7', '.', '.', '.', '.', '.', '.', '.', '.'}, {'8', '.', '.', '.', '.', '.', '.', '.', '.'}, {'9', '.', '.', '.', '.', '.', '.', '.', '.'}};
+        Assert.assertEquals(true, isValidSudoku(a));
+    }
+
+    public boolean isValidSudoku(char[][] board) {
+
+        boolean[][] rowFlag = new boolean[9][9];
+        boolean[][] colFlag = new boolean[9][9];
+        boolean[][] cellFlag = new boolean[9][9];
+
+        for (int i = 0; i < 9; ++i) {
+            for (int j = 0; j < 9; ++j) {
+                if (board[i][j] >= '1' && board[i][j] <= '9') {
+                    int c = board[i][j] - '1';
+                    if (rowFlag[i][c] || colFlag[c][j] || cellFlag[3 * (i / 3) + j / 3][c]) return false;
+                    rowFlag[i][c] = true;
+                    colFlag[c][j] = true;
+                    cellFlag[3 * (i / 3) + j / 3][c] = true;
+                }
+            }
+        }
+        return true;
+    }
 
 }
