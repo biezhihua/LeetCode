22b4005c5d1442cfe4850f1f28181ce121bd874a
biezhihua <biezhihua@gmail.com>
2018-04-09 20:24:54 +0800

    Update

--- src/com/bzh/leetcode/string/CountAndSay.java
+++ src/com/bzh/leetcode/string/CountAndSay.java
@@ -1,15 +1,52 @@
 package com.bzh.leetcode.string;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class CountAndSay {
 
-    @Test
-    public void test(){
+        @Test
+        public void test() {
+            Assert.assertEquals("1", countAndSay(1));
+            Assert.assertEquals("11", countAndSay(2));
+            Assert.assertEquals("21", countAndSay(3));
+            Assert.assertEquals("1211", countAndSay(4));
+            Assert.assertEquals("111221", countAndSay(5));
+            Assert.assertEquals("312211", countAndSay(6));
+            Assert.assertEquals("13112221", countAndSay(7));
+            Assert.assertEquals("1113213211", countAndSay(8));
+            Assert.assertEquals("31131211131221", countAndSay(9));
+            Assert.assertEquals("13211311123113112211", countAndSay(10));
+        }
 
-    }
+        public String countAndSay(int n) {
 
-    public String countAndSay(int n) {
-        return "";
-    }
+            if (n <= 0) return "-1";
+
+            String result = "1";
+            for (int i = 1; i < n; i++) {
+                StringBuilder builder = new StringBuilder();
+                int index = 0;
+                while (index < result.length()) {
+                    // 记录值
+                    char val = result.charAt(index);
+                    // 记录连续位数
+                    int count = 0;
+
+                    // 查找连续位数，碰到不相同的数字时停止。
+                    while (index < result.length() && result.charAt(index) == val) {
+                        index++;
+                        count++;
+                    }
+
+                    // 追加连续位数到结果中
+                    builder.append(String.valueOf(count));
+                    // 追加连续位数的值到结果中
+                    builder.append(val);
+                }
+                result = builder.toString();
+            }
+            return result;
+        }
+
 }
--- src/com/bzh/leetcode/string/LongestCommonPrefix.java
+++ src/com/bzh/leetcode/string/LongestCommonPrefix.java
@@ -1,15 +1,51 @@
 package com.bzh.leetcode.string;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class LongestCommonPrefix {
 
     @Test
     public void test() {
+        Assert.assertEquals("leet", longestCommonPrefix(new String[]{"leets", "leetcode", "leet", "leets"}));
+    }
 
+    // Horizontal Scanning
+    public String longestCommonPrefix_horizontal(String[] strs) {
+
+        if (strs.length == 0) return "";
+
+        String prefix = strs[0];
+
+        for (int i = 1; i < strs.length; i++) {
+
+            // 找出S1与Si间的最长公共字符串
+            // indexOf：当存在串时返回>0；不存串时返回-1
+            // 只要不存在串，就缩减串的规模，再进行查找
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) return "";
+            }
+        }
+        return prefix;
     }
 
     public String longestCommonPrefix(String[] strs) {
-        return "";
+        if (strs == null || strs.length == 0) return "";
+
+        for (int i = 0; i < strs[0].length(); i++) {
+            // 获得第一行i列的字符
+            char c = strs[0].charAt(i);
+
+            // 依次比较剩余行相同列的字符
+            for (int j = 1; j < strs.length; j++) {
+
+                // 若j行i列的字符与第一行i列的字符不同，代表最长公共前缀判定结束
+                // 若第一行新i列已经超出了剩余行的最大列出，也代表最长公共前缀判定结束
+                if (i == strs[j].length() || strs[j].charAt(i) != c)
+                    return strs[0].substring(0, i);
+            }
+        }
+        return strs[0];
     }
 }
