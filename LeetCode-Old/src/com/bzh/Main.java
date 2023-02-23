7b916b6ef437fd49a01faa36f8bc5f2066531802
biezhihua <biezhihua@gmail.com>
2023-02-23 22:45:07 +0800

    rm old

--- LeetCode-Old/src/com/bzh/Main.java
+++
@@ -1,8 +0,0 @@
-package com.bzh;
-
-public class Main {
-
-    public static void main(String[] args) {
-        // Test Update
-    }
-}
--- LeetCode-Old/src/com/bzh/leetcode/tree/SymmetricTree.java
+++
@@ -1,8 +0,0 @@
-package com.bzh.leetcode.tree;
-
-public class SymmetricTree {
-
-    public boolean isSymmetric(TreeNode root) {
-        return false;
-    }
-}
